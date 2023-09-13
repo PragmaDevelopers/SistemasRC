dba7b681f307626ee6f42f2130efbd4123ba0da8
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-09-12 21:31:30 -0300

    made some changes

--- SistemaBase/front-end/src/app/page.tsx
+++ SistemaBase/front-end/src/app/page.tsx
@@ -2,7 +2,26 @@ import { CalendarIcon, ChartPieIcon, CogIcon, ServerStackIcon, UserGroupIcon } f
 import Image from "next/image";
 import Link from "next/link";
 
+interface BoardMenuEntryProps {
+    href: string;
+    name: string;
+    picture: string;
+}
+
+function BoardMenuEntry(props: BoardMenuEntryProps) {
+    return (
+        <Link href={props.href}>
+            <div>
+                <Image src={props.picture} alt={props.name} width={640} height={640} />
+            </div>
+            <h1>{props.name}</h1>
+        </Link>
+    );
+}
+
 export default function Page() {
+    const IconStyles: string = "w-8 aspect-square";
+
     return (
         <main>
             <div>
@@ -13,19 +32,19 @@ export default function Page() {
                     <summary></summary>
                     <div>
                         <Link href="">
-                            <UserGroupIcon />
+                            <UserGroupIcon className={IconStyles} />
                             <h1>Usuários</h1>
                         </Link>
                         <Link href="">
-                            <ServerStackIcon />
+                            <ServerStackIcon className={IconStyles} />
                             <h1>Areas de Trabalho</h1>
                         </Link>
                         <Link href="">
-                            <ChartPieIcon />
+                            <ChartPieIcon className={IconStyles} />
                             <h1>Relatorios</h1>
                         </Link>
                         <Link href="">
-                            <CalendarIcon />
+                            <CalendarIcon className={IconStyles} />
                             <h1>Caléndario</h1>
                         </Link>
                     </div>
@@ -33,34 +52,26 @@ export default function Page() {
                 <details>
                     <summary></summary>
                     <div>
-                        <Link href="">
-                            <div>
-                                <h1>Foto</h1>
-                            </div>
-                            <h1>Nome</h1>
-                        </Link>
-                        <Link href="">
-                            <div>
-                                <h1>Foto</h1>
-                            </div>
-                            <h1>Nome</h1>
-                        </Link>
-                        <Link href="">
-                            <div>
-                                <h1>Foto</h1>
-                            </div>
-                            <h1>Nome</h1>
-                        </Link>
-                        <Link href="">
-                            <div>
-                                <h1>Foto</h1>
-                            </div>
-                            <h1>Nome</h1>
-                        </Link>
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
+                        <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
                     </div>
                 </details>
                 <div>
-                    <CogIcon />
+                    <CogIcon className={IconStyles} />
                     <h1>Configurações</h1>
                 </div>
             </div>
