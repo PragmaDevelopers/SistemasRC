afb3a7cb17b0ec659ba42f4ccfe88a1e3f3068bf
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-09-12 21:37:50 -0300

    made some changes

--- SistemaBase/front-end/src/app/page.tsx
+++ SistemaBase/front-end/src/app/page.tsx
@@ -10,48 +10,46 @@ interface BoardMenuEntryProps {
 
 function BoardMenuEntry(props: BoardMenuEntryProps) {
     return (
-        <Link href={props.href}>
-            <div>
-                <Image src={props.picture} alt={props.name} width={640} height={640} />
-            </div>
+        <Link href={props.href} className="my-2">
+            <Image src={props.picture} alt={props.name} width={640} height={640} className="w-8 aspect-square rounded-md overflow-hidden" />
             <h1>{props.name}</h1>
         </Link>
     );
 }
 
 export default function Page() {
-    const IconStyles: string = "w-8 aspect-square";
+    const IconStyles: string = "w-8 aspect-square mr-2";
 
     return (
-        <main>
-            <div>
+        <main className="w-full h-full overflow-x-hidden flex-col items-start justify-start">
+            <div className="relative w-48 h-full flex flex-col justify-start items-start">
                 <form>
                     <input type="text" placeholder="Pesquisar Card" />
                 </form>
-                <details>
-                    <summary></summary>
-                    <div>
-                        <Link href="">
+                <details className="p-2">
+                    <summary>Seções</summary>
+                    <div className="overflow-x-hidden overflow-y-auto">
+                        <Link href="/" className="flex flex-row justify-between items-center">
                             <UserGroupIcon className={IconStyles} />
                             <h1>Usuários</h1>
                         </Link>
-                        <Link href="">
+                        <Link href="/" className="flex flex-row justify-between items-center">
                             <ServerStackIcon className={IconStyles} />
                             <h1>Areas de Trabalho</h1>
                         </Link>
-                        <Link href="">
+                        <Link href="/" className="flex flex-row justify-between items-center">
                             <ChartPieIcon className={IconStyles} />
                             <h1>Relatorios</h1>
                         </Link>
-                        <Link href="">
+                        <Link href="/" className="flex flex-row justify-between items-center">
                             <CalendarIcon className={IconStyles} />
                             <h1>Caléndario</h1>
                         </Link>
                     </div>
                 </details>
-                <details>
-                    <summary></summary>
-                    <div>
+                <details className="p-2">
+                    <summary>Areas de Trabalho</summary>
+                    <div className="overflow-x-hidden overflow-y-auto">
                         <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
                         <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
                         <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
@@ -70,12 +68,12 @@ export default function Page() {
                         <BoardMenuEntry href="/" picture="https://cdn.ebaumsworld.com/mediaFiles/picture/1151541/84693449.png" name="nome" />
                     </div>
                 </details>
-                <div>
+                <Link href="/" className="absolute bottom-2 flex flex-row justify-between items-center">
                     <CogIcon className={IconStyles} />
                     <h1>Configurações</h1>
-                </div>
+                </Link>
             </div>
-            <div>
+            <div className="grow">
             </div>
         </main>
     );
