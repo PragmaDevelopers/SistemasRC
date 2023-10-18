3f92409ef8fbe3d6d7cf352439540a7991fb1d20
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-10-18 17:29:21 -0300

    made some changes

--- dynamic-form/src/app/(public)/signup/components/form/InputSelect/InputSelect.tsx
+++ dynamic-form/src/app/(public)/signup/components/form/InputSelect/InputSelect.tsx
@@ -1,11 +1,11 @@
-import { UseFormRegister,UseFormWatch } from "react-hook-form";
+import { UseFormRegister, UseFormWatch } from "react-hook-form";
 import InputsInterface from "../Interface/InputsInterface";
 import issuingBodies from "../../../../../../../api/issuingBody/issuingBody";
 import states from "../../../../../../../api/states/states";
 import cities from "../../../../../../../api/cities/cities";
 import neighborhoods from "../../../../../../../api/neighborhoods/neighborhoods";
 import addressTypes from "../../../../../../../api/addressType/addressType";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 type SelectInterface = {
     register: UseFormRegister<InputsInterface>,
@@ -16,9 +16,9 @@ type SelectInterface = {
     apiNeighborhood?: string
 }
 
-export function PowerOfAttorney({register,marginBottom}:SelectInterface){
+export function PowerOfAttorney({ register, marginBottom }: SelectInterface) {
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             <label htmlFor="input-power-attorney">Procuração: </label>
             <select required defaultValue="default" id="input-power-attorney" {...register("power_of_attorney")}>
                 <option disabled value="default">-- Escolha um tipo de procuração --</option>
@@ -31,9 +31,9 @@ export function PowerOfAttorney({register,marginBottom}:SelectInterface){
     )
 }
 
-export function Ocuppation({register,marginBottom}:SelectInterface){
+export function Ocuppation({ register, marginBottom }: SelectInterface) {
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             {/* Futuramente criar um select com várias opções de cargo */}
             <label htmlFor="input-ocuppation">Profissão: </label>
             <input required type="text" id="input-ocuppation" {...register("ocuppation")} />
@@ -41,9 +41,9 @@ export function Ocuppation({register,marginBottom}:SelectInterface){
     )
 }
 
-export function Nationality({register,marginBottom}:SelectInterface){
+export function Nationality({ register, marginBottom }: SelectInterface) {
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             {/* PRIMEIRA FORMA DE SELECIONAR OS DADOS. MAIS FLEXIVEL */}
             <label htmlFor="input-nationality">Nacionalidade: </label>
             <select required defaultValue="default" id="input-nationality" {...register("nationality")}>
@@ -64,9 +64,9 @@ export function Nationality({register,marginBottom}:SelectInterface){
     )
 }
 
-export function MaritalStatus({register,marginBottom}:SelectInterface){
+export function MaritalStatus({ register, marginBottom }: SelectInterface) {
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             <label htmlFor="input-marital-status">Estado Civil: </label>
             <select required defaultValue="default" id="input-marital-status" {...register("marital_status")}>
                 <option disabled value="default">-- Escolha um Estado Civil --</option>
@@ -79,47 +79,47 @@ export function MaritalStatus({register,marginBottom}:SelectInterface){
     )
 }
 
-export function UfForRG({register,marginBottom}:SelectInterface){
+export function UfForRG({ register, marginBottom }: SelectInterface) {
     //UF é para as informações do documentos
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             <label htmlFor="input-UF">UF: </label>
             <select required defaultValue="default" id="input-UF" {...register("uf_for_RG_id")}>
                 <option disabled value="default">-- Escolha um Estado --</option>
-                {states.map(state=>{
-                    return <option key={state.id} value={state.id}>{state.abbreviation} - {state.name}</option>   
+                {states.map(state => {
+                    return <option key={state.id} value={state.id}>{state.abbreviation} - {state.name}</option>
                 })}
             </select>
         </div>
     )
 }
 
-export function IssuingBody({register,marginBottom}:SelectInterface){
+export function IssuingBody({ register, marginBottom }: SelectInterface) {
     return (
         <div style={{ marginBottom: marginBottom }}>
             <label htmlFor="input-issuing-body">Órgão Emissor: </label>
             <select required defaultValue="default" id="input-issuing-body" {...register("issuing_body_id")}>
                 <option disabled value="default">-- Escolha um Órgão Emissor --</option>
                 {issuingBodies.map((issuingBody) => {
-                return (
-                    <option key={issuingBody.abbreviation + issuingBody.name} value={issuingBody.id}>
-                    {issuingBody.abbreviation} - {issuingBody.name}
-                    </option>
-                );
+                    return (
+                        <option key={issuingBody.abbreviation + issuingBody.name} value={issuingBody.id}>
+                            {issuingBody.abbreviation} - {issuingBody.name}
+                        </option>
+                    );
                 })}
             </select>
         </div>
     )
 }
 
-export function StateForAddress({register,watch,apiUf,marginBottom}:SelectInterface){
+export function StateForAddress({ register, watch, apiUf, marginBottom }: SelectInterface) {
     //State é para as informações para endereço
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             <label htmlFor="input-state">Estado: </label>
             <select disabled={watch ? !watch().cepNotFound : false} defaultValue="default" required {...register("state_for_address_id")}>
                 <option disabled value="default">-- Escolha um Estado --</option>
-                {states.map(state=>{
+                {states.map(state => {
                     return <option selected={apiUf ? apiUf == state.abbreviation : false}
                         key={state.id} value={state.abbreviation}>{state.abbreviation} - {state.name}</option>
                 })}
@@ -128,22 +128,22 @@ export function StateForAddress({register,watch,apiUf,marginBottom}:SelectInterf
     )
 }
 
-export function City({register,watch,apiCity,marginBottom}:SelectInterface){
+export function City({ register, watch, apiCity, marginBottom }: SelectInterface) {
     const cepNotFound = watch ? !watch().cepNotFound : false;
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             <label htmlFor="input-city">Cidade: </label>
-            <input value={cepNotFound ? apiCity : watch && watch().city_id} disabled={cepNotFound} type="search" id="input-city" {...register("city_id",{required:true})} />      
+            <input value={cepNotFound ? apiCity : watch && watch().city_id} disabled={cepNotFound} type="search" id="input-city" {...register("city_id", { required: true })} />
         </div>
     )
 }
 
-export function Neighborhood({register,watch,apiNeighborhood,marginBottom}:SelectInterface){
+export function Neighborhood({ register, watch, apiNeighborhood, marginBottom }: SelectInterface) {
     const cepNotFound = watch ? !watch().cepNotFound : false;
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             <label htmlFor="input-neighborhood">Bairro: </label>
-            <input value={cepNotFound ? apiNeighborhood : watch && watch().neighborhood_id} disabled={cepNotFound} type="search" id="input-neighborhood" {...register("neighborhood_id",{required:true})} />      
+            <input value={cepNotFound ? apiNeighborhood : watch && watch().neighborhood_id} disabled={cepNotFound} type="search" id="input-neighborhood" {...register("neighborhood_id", { required: true })} />
         </div>
     )
 }
@@ -172,15 +172,15 @@ export function Neighborhood({register,watch,apiNeighborhood,marginBottom}:Selec
 //     )
 // }
 
-export function UfForCTPS({register,marginBottom}:SelectInterface){
+export function UfForCTPS({ register, marginBottom }: SelectInterface) {
     //UF é para as informações de CTPS
     return (
-        <div style={{marginBottom: marginBottom}}>
+        <div style={{ marginBottom: marginBottom }}>
             <label htmlFor="input-ctps-uf">UF: </label>
             <select defaultValue="default" required id="input-ctps-uf" {...register("uf_for_ctps")}>
                 <option disabled value="default">-- Escolha um Estado --</option>
-                {states.map(state=>{
-                    return <option key={state.id} value={state.id}>{state.abbreviation} - {state.name}</option>   
+                {states.map(state => {
+                    return <option key={state.id} value={state.id}>{state.abbreviation} - {state.name}</option>
                 })}
             </select>
         </div>
